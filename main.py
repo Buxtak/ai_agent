76aaad9b385df41c23d84f950abbbaa5f253c93a
Buxtak <buxtak@gmail.com>
2026-03-12 17:45:28 -0500

    B: Add user input via argparse

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,7 +18,7 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=args.user_prompt,
 )
 if response.usage_metadata is None:
     raise RuntimeError(
