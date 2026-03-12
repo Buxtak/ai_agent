a74658e409766a93328c7a351e8c6619e62421d2
Buxtak <buxtak@gmail.com>
2026-03-12 18:11:08 -0500

    C: send prompts as messages and add verbose CLI output

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,16 +22,16 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt,
+    contents=messages,
 )
 if response.usage_metadata is None:
     raise RuntimeError(
         "API request failed: usage_metadata is missing from the response."
     )
-
-prompt_tokens = response.usage_metadata.prompt_token_count
-candidate_tokens = response.usage_metadata.candidates_token_count
-
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {candidate_tokens}")
-print(response.text)
+if args.verbose is True:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
+else:
+    print(f"Response: {response.text}")
